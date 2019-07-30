54ae9c11e05e33e0e30b6ef31352fc71e92b67c3
balognaBandit <53355533+balognaBandit@users.noreply.github.com>
2019-07-29 20:06:45 -0400

    j

---
+++ P1/Assets/boop3.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boop3 : MonoBehaviour
+{
+    public void buttonclick()
+    {
+        Debug.Log("unu");
+    }
+}
---
+++ P1/Assets/uwu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uwu : MonoBehaviour
+{
+    public void buttonclick()
+    {
+        Debug.Log("uwu");
+    }
+}
